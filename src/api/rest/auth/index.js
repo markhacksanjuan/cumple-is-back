02d63fc3c99a850375d829c661cba631f1a59ed9
Mark <mark.sanjuan.sole@gmail.com>
2022-08-06 13:07:20 +0200

    changed route

--- src/api/rest/auth/index.js
+++ src/api/rest/auth/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const controller = require('./controller')
 
-router.post('/', controller.index)
+router.get('/', controller.index)
 router.post('/signup', controller.signup)
 router.post('/login', controller.login)
 
