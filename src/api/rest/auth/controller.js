994d2e06170be5f32b8ff9a066e563f61f55d7fa
Mark <mark.sanjuan.sole@gmail.com>
2022-08-09 20:18:18 +0200

    chore index

--- src/api/rest/auth/controller.js
+++ src/api/rest/auth/controller.js
@@ -40,28 +40,24 @@ module.exports.login = async (req, res, next) => {
     console.log('login page')
     console.log('starting passport login strategy')
     passport.authenticate('login', (err, user, info) => {
-        try {
-            if(err){
-                res.status(500).send({ errorMessage: 'Something went wrong authentication user' })
-                return
-            }
-            if(!user){
-                res.send(info)
-                return
-            }
-            req.login(
-                user,
-                { session: false },
-                error => {
-                    if(error) {
-                        res.status(400).send({ errorMessage: 'Something went wrong when login' })
-                        return next(error)
-                    }
-                    return res.send({ user, message: info.message })
-                }
-            )
-        }catch(err) {
-            next(err)
+        if(err){
+            res.status(500).send({ errorMessage: 'Something went wrong authentication user' })
+            return
         }
+        if(!user){
+            res.send(info)
+            return
+        }
+        req.login(
+            user,
+            { session: false },
+            error => {
+                if(error) {
+                    res.status(400).send({ errorMessage: 'Something went wrong when login' })
+                    return next(error)
+                }
+                return res.send({ user, message: info.message })
+            }
+        )
     })(req, res, next)
 }
