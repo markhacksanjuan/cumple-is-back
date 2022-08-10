a69bf656ccc72ffaa338e81ab5b82f578171dfc4
Mark <mark.sanjuan.sole@gmail.com>
2022-08-10 02:43:51 +0200

    passport

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -22,7 +22,7 @@ passport.use(
                 console.log(userExists)
                 if(userExists) return done(null, false, { message: 'El usuario ya existe' })
     
-                const salt = await bcrypt.gentSalt(10)
+                const salt = await bcrypt.genSalt(10)
                 const hash = await bcrypt.hash(password, salt)
                 const user = await User.create({ name, password: hash})
     
